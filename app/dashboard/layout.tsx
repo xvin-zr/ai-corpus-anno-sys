f90cf6f7f25b4dd8116d4fe79e314e17522bb99a
xvin <xvin@bupt.edu.cn>
2023-11-28 16:13:05 +0800

    Update sidebar width

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -4,7 +4,7 @@ import SideNav from "./SideNav";
 function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-      <div className="w-full flex-none md:w-52 p-3 pl-2 pb-0 pt-0">
+      <div className="w-full flex-none md:w-60 p-3 pl-2 pb-0 pt-0">
         <SideNav />
       </div>
       <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
