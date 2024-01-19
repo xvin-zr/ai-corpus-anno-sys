9084b6a623f96c11c86102c98aee00653948d0eb
xvin <xvin@bupt.edu.cn>
2024-01-19 20:18:19 +0800

    Update fetchBalance

--- app/dashboard/profile/page.tsx
+++ app/dashboard/profile/page.tsx
@@ -1,24 +1,25 @@
 import { authOptions } from "@/app/api/auth/[...nextauth]/auth-option";
 import prisma from "@/prisma/client";
+import { User } from "@prisma/client";
 import { getServerSession } from "next-auth";
 import ProfileBtn from "./ProfileBtn";
 
 async function fetchBalance(): Promise<number> {
   const session = await getServerSession(authOptions);
   const user = session?.user;
-  if (!user?.email) return -1;
+  if (!user?.email) return NaN;
   try {
-    const foundUser = await prisma.user.findUnique({
+    const { balance } = (await prisma.user.findUnique({
       where: {
         email: user.email,
       },
-    });
-    if (foundUser?.balance) return foundUser.balance.toNumber();
-    else return -1;
+    })) as User;
+    if (balance) return balance.toNumber();
+    else return NaN;
   } catch (error) {
     console.error(error);
+    return NaN;
   }
-  return -1;
 }
 
 async function ProfilePage() {
