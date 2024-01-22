320d0adccc675c5d67cdc924c016971a57e8db1e
xvin <xvin@bupt.edu.cn>
2024-01-22 21:22:40 +0800

    Add Carousel component and update MissionDetailPage

---
+++ app/dashboard/market/[missionId]/Carousel.tsx
@@ -0,0 +1,59 @@
+"use client";
+import clsx from "clsx";
+import Image from "next/image";
+import { useState } from "react";
+import { ChevronLeft, ChevronRight } from "react-feather";
+
+function Carousel({ imageUrls }: { imageUrls: string[] }) {
+  const [currIndex, setCurrIndex] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+
+  function prevSlide() {
+    setIsLoading(true);
+    const newIndex = currIndex == 0 ? imageUrls.length - 1 : currIndex - 1;
+    setCurrIndex(newIndex);
+  }
+  function nextSlide() {
+    setIsLoading(true);
+    const newIndex = currIndex == imageUrls.length - 1 ? 0 : currIndex + 1;
+    setCurrIndex(newIndex);
+  }
+
+  return (
+    <div className="flex h-full w-full items-center">
+      <button
+        className="flex hover:text-v-success dark:hover:text-v-success-light"
+        onClick={prevSlide}
+      >
+        <ChevronLeft size={48} />
+      </button>
+
+      <figure aria-label="mission-images" className="relative h-full w-full">
+        <span
+          className={clsx(
+            "loading loading-spinner loading-lg absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2",
+            { hidden: !isLoading },
+          )}
+        ></span>
+        <Image
+          className="rounded-xl object-cover"
+          src={imageUrls[currIndex]}
+          sizes="100%"
+          fill
+          alt="Mission Images"
+          quality={70}
+          onLoad={() => setIsLoading(false)}
+        />
+      </figure>
+
+      <button
+        className="flex hover:text-v-success dark:hover:text-v-success-light"
+        onClick={nextSlide}
+      >
+        <ChevronRight size={48} />
+      </button>
+    </div>
+  );
+}
+
+export default Carousel;
--- app/dashboard/market/[missionId]/page.tsx
+++ app/dashboard/market/[missionId]/page.tsx
@@ -1,8 +1,14 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/auth-option";
 import prisma from "@/prisma/client";
-import { heading1Style } from "../../components/header.style";
-import { notFound } from "next/navigation";
+import clsx from "clsx";
+import { getServerSession } from "next-auth";
 import Link from "next/link";
-import { ChevronLeft } from "react-feather";
+import { notFound } from "next/navigation";
+import { ArrowLeft } from "react-feather";
+import { heading1Style } from "../../components/header.style";
+import Carousel from "./Carousel";
+
+const tdStyle = clsx("border border-zinc-300 p-3 dark:border-zinc-600");
 
 async function MissionDetailPage({
   params: { missionId },
@@ -14,6 +20,13 @@ async function MissionDetailPage({
     notFound();
   }
 
+  const session = await getServerSession(authOptions);
+  if (!session || !session.user) {
+    notFound();
+  }
+  const userEmail = session.user.email;
+  console.log(userEmail);
+
   return (
     <>
       <h1 className={`${heading1Style} -ml-6 flex items-center gap-1`}>
@@ -21,18 +34,84 @@ async function MissionDetailPage({
           href={"/dashboard/market"}
           className="inline-block hover:text-v-success dark:hover:text-v-success-light"
         >
-          <ChevronLeft size={36} />
+          <ArrowLeft size={38} />
         </Link>
         任务详情
       </h1>
 
-      <p>{mission.title}</p>
-      <p>{mission.createdAt.toLocaleDateString("zh-CN")}</p>
-      <p>{mission.reward?.toNumber()}</p>
-      <p>{mission.description}</p>
-      {mission.images.map((img) => (
-        <p key={img.url}>{img.url}</p>
-      ))}
+      <div className="flex w-full">
+        <div className="rounded-md border border-zinc-300 dark:border-zinc-600">
+          <table className="table-zebra max-w-md flex-none table-fixed border-collapse text-lg">
+            <tbody className="">
+              <tr className="">
+                <td className={[tdStyle, "border-l-0 border-t-0"].join(" ")}>
+                  任务标题
+                </td>
+                <td className={[tdStyle, "border-r-0 border-t-0"].join(" ")}>
+                  {mission.title}
+                </td>
+              </tr>
+              <tr>
+                <td className={[tdStyle, "border-l-0"].join(" ")}>创建时间</td>
+                <td className={[tdStyle, "border-r-0"].join(" ")}>
+                  {mission.createdAt.toLocaleDateString("zh")}
+                </td>
+              </tr>
+              <tr>
+                <td className={tdStyle + " border-l-0"}>报酬</td>
+                <td className={[tdStyle, "border-r-0"].join(" ")}>
+                  ¥&nbsp;{mission.reward?.toNumber()}
+                </td>
+              </tr>
+              <tr>
+                <td className={tdStyle + " border-l-0"}>图片数量</td>
+                <td className={[tdStyle, "border-r-0"].join(" ")}>
+                  {mission.images.length}
+                </td>
+              </tr>
+              <tr>
+                <td className={[tdStyle, "border-b-0 border-l-0"].join(" ")}>
+                  描述
+                </td>
+                <td className={[tdStyle, "border-b-0 border-r-0"].join(" ")}>
+                  <textarea
+                    className="border-none bg-transparent p-0"
+                    style={{ resize: "none" }}
+                    cols={23}
+                    rows={4}
+                    maxLength={50}
+                    defaultValue={mission.description || ""}
+                    disabled
+                  ></textarea>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <div
+          aria-label="image-gallery"
+          className="ml-auto w-[560px] py-4 pl-20"
+        >
+          <Carousel
+            imageUrls={mission.images.map(function getUrl(img) {
+              return img.url;
+            })}
+          />
+        </div>
+      </div>
+
+      <form className="mt-16 flex justify-center">
+        <button
+          className="flex h-10 items-center justify-center gap-1 rounded-md bg-blue-bupt px-6 text-base font-medium text-zinc-50 shadow transition-colors hover:bg-blue-bupt/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-zinc-950 disabled:pointer-events-none disabled:opacity-50 group-invalid:pointer-events-none group-invalid:opacity-50 dark:bg-v-success-dark dark:hover:bg-v-success dark:focus-visible:ring-zinc-300"
+          // disabled={pending}
+          // aria-disabled={pending}
+          // formAction={publishMissionAction}
+        >
+          接受任务
+          {/* {<span className="loading loading-spinner loading-xs" />} */}
+        </button>
+      </form>
     </>
   );
 }
@@ -50,6 +129,7 @@ async function fetchMissionDetail(missionId: string) {
         createdAt: true,
         description: true,
         reward: true,
+        publisherEmail: true,
         images: {
           select: {
             url: true,
