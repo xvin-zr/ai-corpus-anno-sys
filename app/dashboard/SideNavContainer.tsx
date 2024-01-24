c0a216145d1fe95c6b2d5de5cd8575dfdc9e4c72
xvin <xvin@bupt.edu.cn>
2024-01-24 22:12:14 +0800

    Refactor SideNavContainer component display

--- app/dashboard/SideNavContainer.tsx
+++ app/dashboard/SideNavContainer.tsx
@@ -1,13 +1,20 @@
 "use client";
+import clsx from "clsx";
 import { usePathname } from "next/navigation";
 import React from "react";
 
 function SideNavContainer({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
+  const isAnnotating = pathname.includes("/anno");
 
   return (
-    <nav className="w-full flex-none bg-zinc-100 p-3 pb-0 pl-2 pt-0 dark:bg-zinc-800 md:w-60">
-      {children}
+    <nav
+      className={clsx(
+        "w-full flex-none bg-zinc-100 p-3 pb-0 pl-2 pt-0 dark:bg-zinc-800 md:w-60",
+        { hidden: isAnnotating },
+      )}
+    >
+      {!isAnnotating && children}
     </nav>
   );
 }
