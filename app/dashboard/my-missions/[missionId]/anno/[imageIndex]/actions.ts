b33be6fb228a8ecba202218e8a4ed10b602f609f
xvin <xvin@bupt.edu.cn>
2024-02-05 23:03:43 +0800

    Add completeMissionAction function to handle completing a mission

--- app/dashboard/my-missions/[missionId]/anno/[imageIndex]/actions.ts
+++ app/dashboard/my-missions/[missionId]/anno/[imageIndex]/actions.ts
@@ -27,3 +27,35 @@ export async function uploadW3cAnnoAction(
     throw new Error("error in upload w3c annotations");
   }
 }
+
+export async function completeMissionAction(
+  w3cAnnos: W3CAnno[],
+  imageId: string,
+  missionId: string,
+): Promise<boolean> {
+  try {
+    await uploadW3cAnnoAction(w3cAnnos, imageId);
+    await prisma.w3CAnnotation.deleteMany({
+      where: {
+        w3cAnnotations: {
+          equals: [],
+        },
+      },
+    });
+    const updatedMission = await prisma.mission.update({
+      where: {
+        id: missionId,
+      },
+      data: {
+        status: "PENDING_REVIEW",
+      },
+    });
+    if (!updatedMission) {
+      throw new Error("mission not found");
+    }
+    return true;
+  } catch (err) {
+    console.error(err);
+    throw new Error("error in complete mission");
+  }
+}
