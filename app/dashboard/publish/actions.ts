26bd9b99da0f5f34cd273609166102539ecd4b49
xvin <xvin@bupt.edu.cn>
2024-03-22 21:09:48 +0800

    Add file upload functionality and implement review type selection and specified recipient email

--- app/dashboard/publish/actions.ts
+++ app/dashboard/publish/actions.ts
@@ -2,17 +2,23 @@
 
 import { authOptions } from "@/app/api/auth/[...nextauth]/auth-option";
 import prisma from "@/prisma/client";
+import axios from "axios";
 import Decimal from "decimal.js";
 import { getServerSession } from "next-auth";
 import { CldUploadWidgetInfo } from "next-cloudinary";
+import { revalidatePath } from "next/cache";
 import { z } from "zod";
 import { fetchBalance } from "../profile/data";
-import { revalidatePath } from "next/cache";
+import { writeFile } from "fs/promises";
+import path from "path";
 
 interface PublishState {
   success: boolean;
   msg: string;
 }
+
+const acceptFileTypes = ["pdf", "md", "zip", "7z"];
+const OD_SERVER = "http://localhost:3003/api/OD";
 
 export async function publishMission(
   imgs: CldUploadWidgetInfo[],
@@ -22,8 +28,13 @@ export async function publishMission(
   const title = formData.get("title");
   const reward = Number(formData.get("reward"));
   const description = formData.get("description");
+  const reviewType = formData.get("review-type");
+  const insFile = formData.get("instruction-file");
+  const specifiedEmail = formData.get("specified-email");
+
   console.log(title, reward, description);
-  console.log(imgs.map((img) => img.original_filename));
+  console.log(reviewType, insFile);
+  console.log(process.cwd());
 
   const parsed = z
     .object({
@@ -46,6 +57,18 @@ export async function publishMission(
     return { success: false, msg: parsed.error.errors[0].message };
   }
 
+  // 检查指导文件格式
+  if (insFile instanceof File && insFile.size > 0) {
+    if (!acceptFileTypes.includes(insFile.name.split(".").at(-1) ?? "")) {
+      return {
+        success: false,
+        msg: "指导文件格式错误，支持的格式有：pdf, md, zip, 7z",
+      };
+    }
+  }
+
+  // return { success: false, msg: "test" };
+
   try {
     const session = await getServerSession(authOptions);
     if (!session) return { success: false, msg: "请先登录" };
@@ -60,6 +83,46 @@ export async function publishMission(
         success: false,
         msg: `发布失败：当前余额为 ¥${balance}，发布需要 ¥${fee}`,
       };
+    }
+
+    // 检查指定标注人员是否存在
+    if (
+      reviewType === "human" &&
+      typeof specifiedEmail == "string" &&
+      specifiedEmail
+    ) {
+      const user = await prisma.user.findUnique({
+        where: {
+          email: specifiedEmail,
+        },
+      });
+      if (!user) {
+        return {
+          success: false,
+          msg: "指定的标注人员不存在",
+        };
+      }
+    }
+
+    const odRes = (
+      await axios.post(OD_SERVER, {
+        images: imgs.map((img) => {
+          return {
+            url: img.secure_url,
+            id: img.public_id,
+          };
+        }),
+      })
+    )?.data?.results;
+
+    console.log(JSON.stringify(odRes, null, 2));
+
+    if (insFile instanceof File && insFile.size > 0) {
+      const insFileBuffer = Buffer.from(await insFile.arrayBuffer());
+      await writeFile(
+        path.join(process.cwd(), `app/api/insFiles/${insFile.name}`),
+        insFileBuffer,
+      );
     }
 
     const images = imgs.map((img) => {
