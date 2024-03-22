1c525f190a8c900bc046f35a4af108912b1e21fe
xvin <xvin@bupt.edu.cn>
2024-03-22 21:08:56 +0800

    Update next.config.js and package.json

--- next.config.js
+++ next.config.js
@@ -1,22 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "n9fsx5vawnhnz4ce.public.blob.vercel-storage.com",
-          port: "",
-          pathname: "/**",
-        },
-        {
-          protocol: "https",
-          hostname: "res.cloudinary.com",
-          port: "",
-          pathname: "/**",
-        },
-      ],
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "n9fsx5vawnhnz4ce.public.blob.vercel-storage.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+  // experimental: {
+  //   serverComponentsExternalPackages: ["sharp", "onnxruntime-node"],
+  // },
+  webpack: (config) => {
+    // See https://webpack.js.org/configuration/resolve/#resolvealias
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      sharp$: false,
+      "onnxruntime-node$": false,
+    };
+    return config;
+  },
+};
+
+module.exports = nextConfig;
