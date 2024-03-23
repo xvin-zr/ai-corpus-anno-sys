5a349ed79fdac2f3c0ff17ef815af1b0e9136a89
xvin <xvin@bupt.edu.cn>
2024-03-23 15:34:24 +0800

    Add server url and Box type

---
+++ constants/json/index.ts
@@ -0,0 +1,10 @@
+export const MAX_ALLOWED_RECIPIENTS = 3;
+
+export const SERVER_URL = "http://localhost:3000";
+
+export type Box = {
+  xmin: number;
+  ymin: number;
+  xmax: number;
+  ymax: number;
+};
