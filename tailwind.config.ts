6faa1651df000185326bf8d2773387665c4d225e
xvin <xvin@bupt.edu.cn>
2024-04-15 15:18:17 +0800

    Update tailwind.config.ts with new box shadows, border radius, and font sizes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -127,32 +127,33 @@ const config: Config = {
         },
       },
     },
-    boxShadow: {
-      // light
-      "tremor-input": "0 1px 2px 0 rgb(0 0 0 / 0.05)",
-      "tremor-card":
-        "0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)",
-      "tremor-dropdown":
-        "0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)",
-      // dark
-      "dark-tremor-input": "0 1px 2px 0 rgb(0 0 0 / 0.05)",
-      "dark-tremor-card":
-        "0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)",
-      "dark-tremor-dropdown":
-        "0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)",
-    },
-    borderRadius: {
-      "tremor-small": "0.375rem",
-      "tremor-default": "0.5rem",
-      "tremor-full": "9999px",
-    },
-    fontSize: {
-      "tremor-label": ["0.75rem", { lineHeight: "1rem" }],
-      "tremor-default": ["0.875rem", { lineHeight: "1.25rem" }],
-      "tremor-title": ["1.125rem", { lineHeight: "1.75rem" }],
-      "tremor-metric": ["1.875rem", { lineHeight: "2.25rem" }],
-    },
+
     extend: {
+      boxShadow: {
+        // light
+        "tremor-input": "0 1px 2px 0 rgb(0 0 0 / 0.05)",
+        "tremor-card":
+          "0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)",
+        "tremor-dropdown":
+          "0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)",
+        // dark
+        "dark-tremor-input": "0 1px 2px 0 rgb(0 0 0 / 0.05)",
+        "dark-tremor-card":
+          "0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)",
+        "dark-tremor-dropdown":
+          "0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)",
+      },
+      borderRadius: {
+        "tremor-small": "0.375rem",
+        "tremor-default": "0.5rem",
+        "tremor-full": "9999px",
+      },
+      fontSize: {
+        "tremor-label": ["0.75rem", { lineHeight: "1rem" }],
+        "tremor-default": ["0.875rem", { lineHeight: "1.25rem" }],
+        "tremor-title": ["1.125rem", { lineHeight: "1.75rem" }],
+        "tremor-metric": ["1.875rem", { lineHeight: "2.25rem" }],
+      },
       fontFamily: {
         sans: ["var(--font-inter)", "var(--font-noto-sans-sc)"],
       },
